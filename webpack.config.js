59ad914a4ec6eee6750e67c6bf87d1ff9e041f49
Hung <hdv@sphinx-software.com>
2017-10-30 14:32:20 +0700

    add webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,27 @@
 const path              = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-// const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const webpack           = require('webpack');
-const nodeExternals     = require('webpack-node-externals');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-require('dotenv').config();
-const BUILD_DIR    = path.join(__dirname);
-const APP_DIR      = path.join(__dirname, 'app');
+
+// require('dotenv').config();
+const BUILD_DIR    = path.join(__dirname, 'dist');
+const APP_DIR      = path.join(__dirname, 'src');
 const isProduction = (process.argv.indexOf('-p') !== -1);
 
-const VENDOR_LIBS = [
-    'react',
-    'react-dom',
-    'react-redux',
-    'react-helmet',
-    'redux',
-    'redux-form',
-    'react-tap-event-plugin',
-    'react-router',
-    'react-router-config',
-    'react-router-dom',
-    'redux-logger',
-    'material-ui'
-    /**
-     * add more name library
-     */
-];
-const config      = {
+const config = {
     entry  : {
-        bundle: APP_DIR + '/client/index.js',
-        vendor: VENDOR_LIBS
+        bundle: APP_DIR + '/index.js',
+        // vendor: VENDOR_LIBS
     },
     output : {
-        path      : BUILD_DIR + '/public/',
-        // filename: '[name].[chunkhash].js',
+        path      : BUILD_DIR,
         filename  : '[name].js',
-        publicPath: "/",
+        publicPath: "/"
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.js'
+        }
     },
     module : {
         rules: [
@@ -46,24 +31,16 @@ const config      = {
                 exclude: /node_modules/
             },
             {
-                loader: ExtractTextPlugin.extract({
-                    loader: 'css-loader'
-                }),
-                test  : /\.css$/
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/,
-                use : [
-                    {
-                        loader : 'url-loader',
-                        options: {limit: 40000}
-                    },
-                    // 'url-loader',
-                    'image-webpack-loader'
-                ]
+                test  : /\.vue$/,
+                loader: 'vue-loader'
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: APP_DIR + '/index.html'
+        })
+    ]
 };
 
-module.exports = config ;
+module.exports = config;
